b07399b745fea27a35e836d1b48cf71f9d7fbe8a
ku13shov <kuleshov000@gmail.com>
2023-07-11 14:42:53 +0300

    added axios to fetch questions from API

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 
 import Game from './components/Game';
 import Result from './components/Result';
 import './index.scss';
 
-const questions = [
-    {
-        title: 'React - это ... ?',
-        variants: ['библиотека', 'фреймворк', 'приложение'],
-        correct: 0,
-    },
-    {
-        title: 'Компонент - это ... ',
-        variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-        correct: 1,
-    },
-    {
-        title: 'Что такое JSX?',
-        variants: [
-            'Это простой HTML',
-            'Это функция',
-            'Это тот же HTML, но с возможностью выполнять JS-код',
-        ],
-        correct: 2,
-    },
-];
-
 const App = () => {
+    const [questions, setQuestions] = React.useState('');
     const [step, setStep] = React.useState(0);
     const [correct, setCorrect] = React.useState(0);
+
+    React.useEffect(() => {
+        const getQuestions = async () => {
+            try {
+                const { data } = await axios.get('https://4805f9918df2294c.mokky.ru/questions');
+                setQuestions(data);
+            } catch (error) {
+                console.error(error.message);
+            }
+        };
+
+        getQuestions();
+    }, []);
 
     return (
         <div className="App">
             {step < questions.length ? (
-                <Game questions={questions} step={step} setStep={setStep} correct={correct} setCorrect={setCorrect} />
+                <Game
+                    questions={questions}
+                    step={step}
+                    setStep={setStep}
+                    correct={correct}
+                    setCorrect={setCorrect}
+                />
             ) : (
                 <Result correct={correct} questions={questions} />
             )}
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
-
 const Result = ({ correct, questions }) => {
     return (
         <div className="result">
             <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-            <h2>Вы правильно ответили на {correct} вопроса из {questions.length}</h2>
-            <Link >
-            </Link>
-            <button>Попробовать снова</button>
+            <h2>
+                Вы правильно ответили на {correct} вопроса из {questions.length}
+            </h2>
+            <a href="/">
+                <button>Попробовать снова</button>
+            </a>
         </div>
     );
-}
+};
 
 export default Result;
