f34c090d21cb4fa50a32fd0367235ff6dfe0af59
ku13shov <kuleshov000@gmail.com>
2023-07-17 10:41:07 +0300

    fix Result component

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,9 +1,24 @@
 const Result = ({ correct, questions, themeName, themeIndex }) => {
+    const getQuestionEnding = (number) => {
+        const lastDigit = number % 10;
+
+        if (lastDigit === 1) {
+            return 'вопрос';
+        }
+
+        if (lastDigit >= 2 && lastDigit <= 4) {
+            return 'вопроса';
+        }
+
+        return 'вопросов';
+    };
+
     return (
         <div className="result">
             <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="result icon" />
             <h2>
-                Вы правильно ответили на {correct} вопроса из {questions[themeIndex][`${themeName}`].length}
+                Вы правильно ответили на {correct} {getQuestionEnding(correct)} из{' '}
+                {questions[themeIndex][`${themeName}`].length}
             </h2>
             <a href="/">
                 <button>Попробовать снова</button>
